c2305be4893a36502af1e39d2e5c99dd687fb76e
Roman Perepelkin <perepelkinrs@gmail.com>
2024-07-05 08:52:12 +0300

    make build

---
+++ index.29fda1ee.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l="".concat(e.clientWidth/2,"px");t.style.top=l,t.style.left=l,t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.29fda1ee.js.map
